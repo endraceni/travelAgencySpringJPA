466ba6bc0d611666e64736bc894463d51ab7b4b4
endraceni <44263329+endraceni@users.noreply.github.com>
2020-07-01 13:06:58 +0200

    Update RecepieApplication.java

--- src/main/java/ceni/endra/RecepieApplication.java
+++ src/main/java/ceni/endra/RecepieApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RecepieApplication {
+public class TravelAgency {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RecepieApplication.class, args);
+		SpringApplication.run(TravelAgency.class, args);
 	}
 
 }
